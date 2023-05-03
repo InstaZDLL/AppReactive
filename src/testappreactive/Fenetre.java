20581814a255060822f1d98b3ca0245b4cbfae7f
InstaZDLL <github.105mh@8shield.net>
2023-05-03 13:04:07 +0200

    Refactor Fenetre.java to add two button listeners.

--- src/testappreactive/Fenetre.java
+++ src/testappreactive/Fenetre.java
@@ -2,6 +2,8 @@ package testappreactive;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import static java.awt.Color.*;
 import static java.awt.Font.BOLD;
@@ -42,13 +44,36 @@ public class Fenetre extends JFrame {
                 mainPanel.add(label, BorderLayout.NORTH);
 
                 // CENTRE
+                mainPanel.add(centerPanel, BorderLayout.CENTER);
 
                 // SUD
+                southPanel.add(bouton1);
+                    // Ecouteur de bouton 1
+                    bouton1.addActionListener(new Bouton1Listener());
+                southPanel.add(bouton2);
+                    // Ecouteur de bouton 2
+                    bouton2.addActionListener(new Bouton2Listener());
+                mainPanel.add(southPanel, BorderLayout.SOUTH);
 
             // Coller sur le ContentPane
             this.setContentPane(mainPanel);
         // Visible ?
         this.setVisible(true);
     }
+    // Classe écouteurs
+    class Bouton1Listener implements ActionListener {
 
+        @Override
+        public void actionPerformed(ActionEvent evt) {
+            label.setText("Vous avez pressé sur le bouton No 1 !");
+        }
+    }
+
+    class Bouton2Listener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent evt) {
+            label.setText("Vous avez pressé sur le bouton No 2 !");
+        }
+    }
 }
